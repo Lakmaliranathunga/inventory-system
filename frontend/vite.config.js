d6324df9e5ce2d3e6b443fd44b94a4294e8a84ff
Lakmali Ranathunga <lakmaliranathunga98@gmail.com>
2026-09-10 11:59:44 -0800

    Configure frontend for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/inventory-system/',
   server: {
     port: 3000,
     strictPort: true,
